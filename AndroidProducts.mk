7ccf67f7e4b2d18c9fe8dfa98c9b4b2ff49f2bc2
nhAsif <najmulhasan3609@gmail.com>
2024-05-06 05:08:44 +0000

    Alioth: Derpfest-Aosp bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_alioth.mk
+    $(LOCAL_DIR)/derp_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_alioth-user \
-    evolution_alioth-userdebug \
-    evolution_alioth-eng
+    derp_alioth-user \
+    derp_alioth-userdebug \
+    derp_alioth-eng
--- derp_alioth.mk
+++ derp_alioth.mk
@@ -10,20 +10,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Evolution X stuff.
 BUILD_WITH_GAPPS := true
+TARGET_USES_PICO_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_BUILD_APERTURE_CAMERA := false
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_MIUI_CAMERA := true
 TARGET_INCLUDES_MIUI_CAMERA := true
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Build Type Flags
-EVO_BUILD_TYPE = DEVELOPMENT
+DERP_BUILDTYPE := Fan-Edition
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := evolution_alioth
+PRODUCT_NAME := derp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
