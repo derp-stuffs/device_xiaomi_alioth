cc78cd7512aeb5043bb73876ec80e4c6cab2433d
Asif <49162687+nhAsif@users.noreply.github.com>
2024-05-08 07:50:58 +0600

    Alioth: drop ksu

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/configs/camera/st_license.lic
-rm -rf system/extras/su
-cd kernel/xiaomi/sm8250 && git submodule init && git submodule update
-croot
