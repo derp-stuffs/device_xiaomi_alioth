c26b41c389ccd1df8dc529f227e106558f642569
Asif <49162687+nhAsif@users.noreply.github.com>
2024-06-10 10:16:52 +0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,4 @@
 
 base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/configs/camera/st_license.lic
 rm -rf vendor/derp/signing
+git clone https://github.com/los-alioth/keys.git -b derp vendor/derp/signing/keys
